4fe60869ac26cf9ec866aa493881c1fafeb64540
黄锴杰 <1401237226@qq.com>
2018-10-17 02:41:29 +0000

    随车打印程序补提交

---
+++ source/DeskApp/WpfQualityCertPrinter/WpfQualityCertPrinter/Model/PdStockout.cs
@@ -0,0 +1,25 @@
+﻿namespace DataLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("pdstockout")]
+    public partial class PdStockout
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int? Productid { get; set; }
+
+        public int? Sellerid { get; set; }
+
+        [MaxLength(50)]
+        public string Lpn { get; set; }
+
+        public int? Createtime { get; set; }
+
+        public int? Adder { get; set; }
+    }
+}
