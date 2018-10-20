91cf6537a98001f8f6709511d59f1e4a2bdffbc0
黄锴杰 <1401237226@qq.com>
2018-10-20 04:20:25 +0000

    1.新增炉批号和车牌号查询功能

--- source/WebApp/WebSite/Pages/Manage/Quality/Download.cshtml.cs
+++ source/WebApp/WebSite/Pages/Manage/Quality/Download.cshtml.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using System.Threading.Tasks;
     using Common.IService;
     using DataLibrary;
@@ -17,11 +18,13 @@
     public class DownloadModel : AuthorizeModel
     {
         private IBaseService<SalePrintlog> salePrintlog;
+        private Util.Helpers.MySqlHelper mySqlHelper;
 
-        public DownloadModel(DataContext db, IBaseService<SalePrintlog> salePrintlog)
+        public DownloadModel(DataContext db, IBaseService<SalePrintlog> salePrintlog, Util.Helpers.MySqlHelper mySqlHelper)
           : base(db)
         {
             this.salePrintlog = salePrintlog;
+            this.mySqlHelper = mySqlHelper;
         }
 
         public override int PageSize
@@ -30,6 +33,8 @@
         }
 
         public List<DataLibrary.SalePrintlog> SalePrintLogList { get; set; }
+
+        public List<ViewModelLog> ViewModelLogList { get; set; }
 
         public int PageIndex { get; set; }
 
@@ -43,21 +48,32 @@
 
         public new int Page { get; set; }
 
-        public void OnGet(string printno = "", string startTime = "", string endTime = "", int pg = 1)
+        public string BatCode { get; set; }
+
+        public string Lpn { get; set; }
+
+        public void OnGet(string batCode = "", string lpn = "", string printno = "", string startTime = "", string endTime = "", int pg = 1)
         {
             this.PageIndex = pg;
-            var predicate = PredicateBuilder.New<SalePrintlog>(true);
-            predicate.Extend(
-                w => w.Status == (int)SalePrintlogStatus.已下载 ||
-                w.Status == (int)SalePrintlogStatus.已撤回, PredicateOperator.And);
 
-            int total = 0;
+            // var predicate = PredicateBuilder.New<SalePrintlog>(true);
+            // predicate.Extend(
+            //    w => w.Status == (int)SalePrintlogStatus.已下载 ||
+            //    w.Status == (int)SalePrintlogStatus.已撤回, PredicateOperator.And);
+            var sqlStr = new StringBuilder();
+            sqlStr.Append(" select a.*,c.Batcode,c.Lpn from saleprintlog  a ");
+            sqlStr.Append(" inner JOIN saleprintlogdetail b on a.id=b.printid ");
+            sqlStr.Append(" inner JOIN salesellerauth c on b.authid=c.Id ");
+            sqlStr.AppendFormat(" where (a.status ={0} or a.status={1}) ", (int)SalePrintlogStatus.已下载, (int)SalePrintlogStatus.已撤回);
+
+            // int total = 0;
             if (!string.IsNullOrEmpty(printno))
             {
                 this.Printno = printno;
+                sqlStr.AppendFormat(" and  a.printno ='{0}' ", printno);
 
                 // 添加查询条件,PredicateOperator.And查询谓词
-                predicate.Extend(w => w.Printno == printno, PredicateOperator.And);
+                // predicate.Extend(w => w.Printno == printno, PredicateOperator.And);
             }
 
             if (!DateTime.TryParse(startTime, out DateTime sTiem))
@@ -67,9 +83,10 @@
             else
             {
                 this.StartTime = startTime;
+                sqlStr.AppendFormat(" and a.createtime >= {0} ", sTiem.GetUnixTimeFromDateTime());
 
                 // 添加查询条件,PredicateOperator.And查询谓词
-                predicate.Extend(w => System.Convert.ToDateTime(w.Createtime.ToLong().GetDateTimeFromUnixTime().ToString("yyyy-MM-dd")) >= sTiem, PredicateOperator.And);
+                // predicate.Extend(w => System.Convert.ToDateTime(w.Createtime.ToLong().GetDateTimeFromUnixTime().ToString("yyyy-MM-dd")) >= sTiem, PredicateOperator.And);
             }
 
             if (!DateTime.TryParse(endTime, out DateTime eTime))
@@ -79,13 +96,36 @@
             else
             {
                 this.EndTime = endTime;
+                sqlStr.AppendFormat(" and  a.createtime <= {0} ", eTime.GetUnixTimeFromDateTime());
 
                 // 添加查询条件,PredicateOperator.And查询谓词
-                predicate.Extend(w => System.Convert.ToDateTime(w.Createtime.ToLong().GetDateTimeFromUnixTime().ToString("yyyy-MM-dd")) <= eTime, PredicateOperator.And);
+                // predicate.Extend(w => System.Convert.ToDateTime(w.Createtime.ToLong().GetDateTimeFromUnixTime().ToString("yyyy-MM-dd")) <= eTime, PredicateOperator.And);
             }
 
-            this.SalePrintLogList = this.salePrintlog.Page<int>(ref total, this.PageIndex, this.PageSize, p => p.Id, predicate, false);
-            this.PageCount = total;
+            if (!string.IsNullOrEmpty(batCode))
+            {
+                this.BatCode = batCode;
+                sqlStr.AppendFormat(" and  c.Batcode ='{0}' ", batCode);
+            }
+
+            if (!string.IsNullOrEmpty(lpn))
+            {
+                this.Lpn = lpn;
+                sqlStr.AppendFormat(" and  c.lpn like '%{0}%' ", lpn);
+            }
+
+            // this.SalePrintLogList = this.salePrintlog.Page<int>(ref total, this.PageIndex, this.PageSize, p => p.Id, predicate, false);
+            sqlStr.Append(" GROUP BY a.printno,b.number,a.Createtime,a.consignor, a.id,c.Lpn,c.Batcode  ORDER BY  a.id desc ");
+            this.PageCount = this.mySqlHelper.ExecuteList<ViewModelLog>(sqlStr.ToString(), null, System.Data.CommandType.Text).Count;
+            sqlStr.AppendFormat(" LIMIT {0},{1} ", (this.PageIndex - 1) * this.PageSize, this.PageSize);
+            this.ViewModelLogList = this.mySqlHelper.ExecuteList<ViewModelLog>(sqlStr.ToString(), null, System.Data.CommandType.Text);
+        }
+
+        public class ViewModelLog : SalePrintlog
+        {
+            public string BatCode { get; set; }
+
+            public string Lpn { get; set; }
         }
     }
 }
